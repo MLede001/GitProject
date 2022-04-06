c5ca470c822f03e8eed3bce6d317a1aca14f3959
Marie <Marie@DESKTOP-9NO48JI>
2022-04-06 13:52:58 +0700

    commit

---
+++ Test Suites/Test Suites_2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suites_2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0abb2eef-3460-4e82-96ad-5a1e46f36159</testSuiteGuid>
+   <testCaseLink>
+      <guid>f8d15c8a-585e-4175-9fd8-4d4a5cf287a1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case_1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
